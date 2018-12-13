89dff0c9b55c2dd71e0c59e99eea60a23909844f
Mendonca, Tadeu <tadeu.mendonca@outlook.com>
2018-12-13 00:13:27 -0200

    Setting up navigation component.

---
+++ app/src/main/java/com/example/forecastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.forecastmvvm.ui.settings
+
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : Fragment() {
+}
